423d1d83004f8c636a308e693c0227d19505a396
student <student@students-mac.student.georgianc.on.ca>
2016-02-03 15:38:48 -0500

    Added calculator display label and digit buttons

--- iOSCalculatorApp/ViewController.swift
+++ iOSCalculatorApp/ViewController.swift
@@ -8,18 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController
+{
+   //Define Properties
+    @IBOutlet weak var calculatorDisplay: UILabel!
+    
+    
+    
+    @IBAction func digitButton(sender: UIButton)
+    {
+        
     }
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
